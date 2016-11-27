83a98964553a3ca9290c2b2e588feb2328be27fc
vietchinh <vietchinhpham@hotmail.com>
2016-11-27 12:00:44 +0100

    Moved Content to it's new home!

---
+++ array-content.php
@@ -0,0 +1,109 @@
+<?php
+#Random Song
+function randomsong(){
+	require_once "youtube-random.php";
+	$randomsonglink = $randomlink[mt_rand(0, count($randomlink))];
+	return $randomsonglink;
+}
+$randomsong = randomsong();
+#Content
+$home = "
+<div class='panel'>
+	<div class='panel-title'>
+		<h3>Home</h3>
+	</div>
+	<div class='content'>
+	</div>
+</div>`
+";
+$aboutme = "
+<!--Fixing Later, Layout Issue
+<div class='panel-sm'>
+	<div class='panel-title'>
+		<h3>Random Songs From My Playlist</h3>
+	</div>
+	<div class='content-sm'>
+		<iframe src='https://www.youtube.com/embed/{$randomsong}' frameborder='0' allowfullscreen></iframe>
+	</div>
+	<div class='panel-title'>
+		<h3>My Anime List Emblem</h3>
+	</div>
+		<img src='http://www.foxinflame.tk/dev/MALembed/vietchinh?bg_color=FFFFFF&shadow=000000&shadow_amount=2'>
+</div>-->
+<div class='panel'>
+	<div class='panel-title'>
+		<h3>About Me</h3>
+	</div>
+	<div class='content'>
+		<p>
+			hi,
+			<br />
+			I'm Vietchinh or Viet or whatever you want to call me. 
+			<br />
+			I am an introverted gamer. Although I take gamer lightly as I don't game much outside of league of legends. 
+			<br />
+			I watch anime and my origin(or my parents birth place) is vietnam. 
+			<br />
+			I have a twitch stream and a  channel if you are interested. 
+			<br />
+		</p>
+			<h4 class='h5aboutme'>Anime - 'Where fantasy becomes reality and reality becomes fantasy.'</h4>
+			<hr class='hraboutme' />
+		<p>	
+			The type of genres I watch are: slice of life, romance, action, adventure and many more that I may overlooked. <br />
+			I do enjoy slice of life the most for relate able matters. <br />
+			Like how Hishiro, Chizuru from the anime relife is the closest character on my personality. Minus the lack of social skills and being incredibly dense. <br />
+		</p>
+			<h4 class='h5aboutme'>Music - 'Where words fail, Music speaks' - hdsoundi </h4>
+			<hr class='hraboutme' />
+		<p>
+			I have a large collection of different genre of music's, depending on my mood I can choose one of my liking. 
+			<br />
+			genre's like... 
+			<br />
+			- Japanese Songs(like felt from touhou project, aimer and many others..)(no not japanese pop, my goodness) 
+			<br />
+			- EDM 
+			<br />
+			- Electronics 
+			<br />
+			- Chill 
+			<br />
+			- D&B 
+			<br />
+			- Epic Music
+			<br />
+			- Emotional Music (preferred most of the time) 
+			<br />
+			- and the list goes on.. and on.. 
+			<br />
+			I do have some clear favorite's if I watch my play count of my playlist. 
+			<br />
+		</p>
+	</div>
+</div>
+";
+$websitechangelogs = "
+	<div class='panel'>
+		<div class='panel-title'>
+			<button id='right'onclick='right();'> > </button>
+			<button id='left' onclick='left();'> < </button>
+		</div>
+		<div class='content'>
+		</div>
+	</div>
+";
+#End Content
+
+#$Content-Array
+	$content = array (
+	"Home" => array($home, "home"
+	),
+	"About Me" => array($aboutme, "aboutme"
+	),
+	"Website Changelogs" => array($websitechangelogs, "websitechangelogs"
+	)
+	);
+	$randomtext = array ("A Random Text of The day", "You are Awesome!", "SUP!", "Welcome!");
+#End Array
+?>
---
+++ javascript.php
@@ -0,0 +1,117 @@
+<body>
+	<script type="text/javascript">
+	/*--Menu Button Interactions Section--*/
+		var memory = ["#home"];
+		function fade(){
+			previous = memory[0];
+			current = memory[1];	
+			if ($(current).css("display") == "none") {
+				switch (current) {
+				<?php
+					while (current($content)){
+						$contentkey = key($content);
+							echo "
+								case '#{$content[$contentkey][1]}': 
+									$(previous).fadeOut('fast', function(){ $(current).fadeIn('slow').css('display','inline-flex'); memory.splice(0, 1); });
+								break;
+								";
+						next($content);
+					}
+					reset($content);
+				?>
+				}
+			}
+			else {
+				switch (current) {
+					<?php
+						while (current($content)){
+							$contentkey = key($content);
+								echo "
+									case '#{$content[$contentkey][1]}': 
+										$(previous).fadeOut('fast', function(){ $(current).fadein('slow').css('display','inline-flex'); memory.splice(0, 1); });
+									break;
+									";
+							next($content);
+						}
+						reset($content);
+					?>
+				}			
+			}
+		}
+		<?php
+			while (current($content)){
+				$contentkey = key($content);
+				echo "
+					function {$content[$contentkey][1]}(){
+						var index = memory.indexOf('#{$content[$contentkey][1]}');
+						if (memory[index] == '#{$content[$contentkey][1]}'){
+						}
+						else {
+							memory.push('#{$content[$contentkey][1]}');
+							fade();
+						}
+					}
+					";
+				next($content);
+			}
+			reset($content);
+		?>
+	/*--End Menu Button Interactions Section--*/
+	/*--WebsiteChangelogs Interactions Section--*/
+		var title = ["Concept Design", "Intial Design", "Version 1", "Version 2"];
+		var pathtoimg = ["changelog-images/concept design2.jpg", "changelog-images/initial design.jpg", "changelog-images/14-11-2016.jpg", "changelog-images/17-11-2016.jpg"];
+		var number = 0;
+		var h3 = document.createElement("h3");
+		h3.innerHTML = title[0];
+		document.getElementById("websitechangelogs").getElementsByClassName("panel-title")[0].appendChild(h3);
+		var img = document.createElement("img");
+		img.src = pathtoimg[0];
+		document.getElementById("websitechangelogs").getElementsByClassName("content")[0].appendChild(img);
+		function right(){
+			if (number != (title.length - 1)){
+				number = number + 1;
+				console.log(number);
+				fadetitle(number);
+			}
+		}
+		function left(){
+			if (number != 0){
+				number = number - 1;
+				console.log(number);
+				fadetitle(number);
+			}
+		}
+		function fadetitle(number) {
+			if (number != 0){
+				$("#websitechangelogs").children(".panel").children(".panel-title").children("#left").fadeIn("fast");
+			}
+			else {
+				$("#websitechangelogs").children(".panel").children(".panel-title").children("#left").fadeOut("fast");
+			}
+			if (number != (title.length - 1) ){
+				$("#websitechangelogs").children(".panel").children(".panel-title").children("#right").fadeIn("fast");
+			}
+			else {
+				$("#websitechangelogs").children(".panel").children(".panel-title").children("#right").fadeOut("fast");
+			}
+			$("#websitechangelogs").children(".panel").children(".panel-title").children("h3").fadeOut("normal",
+			function() {
+				$(this).remove();
+				var h3 = document.createElement("h3");
+				h3.innerHTML = title[number];
+				h3.style.display = "none";
+				document.getElementById("websitechangelogs").getElementsByClassName("panel-title")[0].appendChild(h3);
+				$(h3).fadeIn();
+			});
+			$("#websitechangelogs").children(".panel").children(".content").children("img").fadeOut("normal",
+			function() {
+				$(this).remove();
+				var img = document.createElement("img");
+				img.src = pathtoimg[number];
+				img.style.display = "none";
+				document.getElementById("websitechangelogs").getElementsByClassName("content")[0].appendChild(img);
+				$(img).fadeIn();
+			});
+		}
+	</script>
+</body>
